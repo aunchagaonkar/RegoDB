a4309650c7195b3e80e8de16f63ecab195639422
aunchagaonkar <ameya21unchagaonkar@gmail.com>
2025-07-25 01:20:45 +0530

    Improve PING response handling

--- app/main.go
+++ app/main.go
@@ -1,24 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Logs from your program will appear here!")
+	fmt.Println("Logs from program will appear here!")
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
 	// accepting a connection to keep the server running
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		// respond to PING
+		go handlePing(conn)
 	}
-	// respond to PING
-	conn.Write([]byte("+PONG\r\n"))
+}
+
+func handlePing(conn net.Conn) {
+	defer conn.Close()
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		test := scanner.Text()
+		if strings.TrimSpace(test) == "PING" {
+			conn.Write([]byte("+PONG\r\n"))
+		}
+	}
 }
